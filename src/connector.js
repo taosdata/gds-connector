0667fa97f734bac81aba5f8ddab98c9ae920335b
xleili <xlli@taosdata.com>
2022-06-18 17:27:04 +0800

    release(driver):format src/connctor.js

--- src/connector.js
+++ src/connector.js
@@ -57,7 +57,6 @@ function getConfig(request) {
         .setName('Enter query range end time')
         .setHelpText('e.g. 2020-04-21 20:53:00')
 
-
     config.setDateRangeRequired(true);
 
     return config.build();
@@ -65,20 +64,20 @@ function getConfig(request) {
 
 
 function getFields(request, cached) {
-    if(request.configParams.TD_URL==undefined){
-      throw new Error("URL should not be empty"+request.configParams.TD_URL);
+    if (request.configParams.TD_URL == undefined) {
+        throw new Error("URL should not be empty" + request.configParams.TD_URL);
     }
-    if(request.configParams.TD_USER==undefined){
-      throw new Error("username should not be empty");
+    if (request.configParams.TD_USER == undefined) {
+        throw new Error("username should not be empty");
     }
-    if(request.configParams.TD_PASSWORD==undefined){
-      throw new Error("passoword should not be empty");
+    if (request.configParams.TD_PASSWORD == undefined) {
+        throw new Error("passoword should not be empty");
     }
-    if(request.configParams.TD_DATABASE==undefined){
-      throw new Error("database should not be empty");
+    if (request.configParams.TD_DATABASE == undefined) {
+        throw new Error("database should not be empty");
     }
-    if(request.configParams.TD_TABLE==undefined){
-      throw new Error("table should not be empty")
+    if (request.configParams.TD_TABLE == undefined) {
+        throw new Error("table should not be empty")
     }
     var cache = CacheService.getScriptCache()
     var cacheKey = [
@@ -97,27 +96,27 @@ function getFields(request, cached) {
             return cachedSchema
         }
     }
- 
-    try{
-        var responseJson=doQUery(request.configParams.TD_URL + '/rest/sqlutc/' + request.configParams.TD_DATABASE, 'describe ' + request.configParams.TD_TABLE,request)
-         }catch(e){
-      throwConnectorError(e.message,true);
+
+    try {
+        var responseJson = doQUery(request.configParams.TD_URL + '/rest/sqlutc/' + request.configParams.TD_DATABASE, 'describe ' + request.configParams.TD_TABLE, request)
+    } catch (e) {
+        throwConnectorError(e.message, true);
     }
-      var fields = cc.getFields();
-      var types = cc.FieldType;
-      for (var i in responseJson.data) {
-          d = responseJson.data[i]
-          var name = d[0]
-          var fieldType = d[1]
-          var colType = d[3]
-          if (colType === "TAG" || fieldType === 9) {
-              fields.newDimension().setId(name).setName(name).setType(getType(fieldType, types))
-          } else {
-              fields.newMetric().setId(name).setName(name).setType(getType(fieldType, types))
-          }
-      }
-   
-    
+    var fields = cc.getFields();
+    var types = cc.FieldType;
+    for (var i in responseJson.data) {
+        d = responseJson.data[i]
+        var name = d[0]
+        var fieldType = d[1]
+        var colType = d[3]
+        if (colType === "TAG" || fieldType === 9) {
+            fields.newDimension().setId(name).setName(name).setType(getType(fieldType, types))
+        } else {
+            fields.newMetric().setId(name).setName(name).setType(getType(fieldType, types))
+        }
+    }
+
+
     var cacheValue = JSON.stringify(fields.build())
     Logger.log(
         'Store cached schema for key: %s, schema: %s',
@@ -129,14 +128,12 @@ function getFields(request, cached) {
 }
 
 function getSchema(request) {
- try{
-    fields = getFields(request, false)
-     }catch(e){
-   throwConnectorError(e.message,true);
- }
+    try {
+        fields = getFields(request, false)
+    } catch (e) {
+        throwConnectorError(e.message, true);
+    }
     return { 'schema': fields };
-
-   
 }
 
 function getType(fieldType, types) {
@@ -167,55 +164,55 @@ function getType(fieldType, types) {
 
 function getData(request) {
     var timeRange = ""
- try {
-    if (request.configParams.TD_START_TIME && request.configParams.TD_END_TIME) {
-        var start = new Date(request.configParams.TD_START_TIME).toISOString()
-        var end = new Date(request.configParams.TD_END_TIME).toISOString()
-        timeRange = " where ts >= '" + start + "'and ts <='" + end + "'"
-    } else {
-        var haveStart = false
-        var haveEnd = false
-        if (request.dateRange && request.dateRange.startDate) {
-            haveStart = true
-            var start = new Date(request.dateRange.startDate).toISOString()
-        }
-        if (request.dateRange && request.dateRange.endDate) {
-            haveEnd = true
-            var end = new Date(request.dateRange.endDate).toISOString()
-        }
-        if (haveStart || haveEnd) {
-            if (haveStart && haveEnd) {
-                timeRange = " where ts >= '" + start + "'and ts <='" + end + "'"
-            } else if (haveStart) {
-                timeRange = " where ts >= '" + start + "'"
-            } else {
-                timeRange = " where ts <= '" + end + "'"
+    try {
+        if (request.configParams.TD_START_TIME && request.configParams.TD_END_TIME) {
+            var start = new Date(request.configParams.TD_START_TIME).toISOString()
+            var end = new Date(request.configParams.TD_END_TIME).toISOString()
+            timeRange = " where ts >= '" + start + "'and ts <='" + end + "'"
+        } else {
+            var haveStart = false
+            var haveEnd = false
+            if (request.dateRange && request.dateRange.startDate) {
+                haveStart = true
+                var start = new Date(request.dateRange.startDate).toISOString()
+            }
+            if (request.dateRange && request.dateRange.endDate) {
+                haveEnd = true
+                var end = new Date(request.dateRange.endDate).toISOString()
+            }
+            if (haveStart || haveEnd) {
+                if (haveStart && haveEnd) {
+                    timeRange = " where ts >= '" + start + "'and ts <='" + end + "'"
+                } else if (haveStart) {
+                    timeRange = " where ts >= '" + start + "'"
+                } else {
+                    timeRange = " where ts <= '" + end + "'"
+                }
             }
         }
-    }
 
-    var names = request.fields.map(function (field) { return field.name })
-    var allFields = getFields(request, true)
-    var allMap = {}
-    var fieldsFiltered = []
-    for (var field of allFields) {
-        allMap[field.name] = field
-    }
-    for (var name of names) {
-        field = allMap[name]
-        if (field) {
-            fieldsFiltered.push(field)
+        var names = request.fields.map(function (field) { return field.name })
+        var allFields = getFields(request, true)
+        var allMap = {}
+        var fieldsFiltered = []
+        for (var field of allFields) {
+            allMap[field.name] = field
         }
-    }
-    var nameStr = names.join(',')
-    
-    var json = doQUery(request.configParams.TD_URL + '/rest/sqlutc/' + request.configParams.TD_DATABASE, "select " + nameStr + " from " + request.configParams.TD_TABLE + timeRange + " limit 1000000",request)
-//    Logger.log("[response]:"+resp)
-//    var body = resp.getContentText();
-//    var json = JSON.parse(body)
-    var rows = []
-    var types = cc.FieldType;
-   
+        for (var name of names) {
+            field = allMap[name]
+            if (field) {
+                fieldsFiltered.push(field)
+            }
+        }
+        var nameStr = names.join(',')
+
+        var json = doQUery(request.configParams.TD_URL + '/rest/sqlutc/' + request.configParams.TD_DATABASE, "select " + nameStr + " from " + request.configParams.TD_TABLE + timeRange + " limit 1000000", request)
+        //    Logger.log("[response]:"+resp)
+        //    var body = resp.getContentText();
+        //    var json = JSON.parse(body)
+        var rows = []
+        var types = cc.FieldType;
+
         for (var i in json.data) {
             var d = []
             for (var j = 0; j < fieldsFiltered.length; j++) {
@@ -248,33 +245,32 @@ function getData(request) {
 
 function doQUery(url, body, request) {
     var cache = CacheService.getScriptCache()
-    var basic = 'Basic '+Utilities.base64Encode(request.configParams.TD_USER + ':' + request.configParams.TD_PASSWORD);
+    var basic = 'Basic ' + Utilities.base64Encode(request.configParams.TD_USER + ':' + request.configParams.TD_PASSWORD);
     Logger.log(JSON.stringify(body));
     var options = {
         method: 'POST',
         payload: body,
         headers: { 'Authorization': basic }
     }
-//    Logger.log(url);
-//    Logger.log(options);
+    //    Logger.log(url);
+    //    Logger.log(options);
 
     var response = UrlFetchApp.fetch(url, options);
-    Logger.log("doQuery:"+response);
-    if(response.getResponseCode() == 200){
-      var body = response.getContentText();
-      var json = JSON.parse(body)
-       if(json.status=='succ'){
-          return json;
-          if(json.rows==0){
-          throw Error("No data is available for the requested time period")
-          }
-       }else{
-         throw Error("Fetch data from TDengine fail,reason:"+json.desc)
-       }
-    }else{
-      throw Error("fetch reqest fail,code:"+response.getResponseCode());
+    Logger.log("doQuery:" + response);
+    if (response.getResponseCode() == 200) {
+        var body = response.getContentText();
+        var json = JSON.parse(body)
+        if (json.status == 'succ') {
+            return json;
+            if (json.rows == 0) {
+                throw Error("No data is available for the requested time period")
+            }
+        } else {
+            throw Error("Fetch data from TDengine fail,reason:" + json.desc)
+        }
+    } else {
+        throw Error("fetch reqest fail,code:" + response.getResponseCode());
     }
-  
 }
 
 /**
@@ -285,11 +281,10 @@ function doQUery(url, body, request) {
  *     otherwise. false by default.
  */
 function throwConnectorError(message, userSafe) {
-  userSafe = (typeof userSafe !== 'undefined' &&
-              typeof userSafe === 'boolean') ? userSafe : false;
-  if (userSafe) {
-    message = 'DS_USER:' + message;
-  }
-
-  throw new Error(message);
+    userSafe = (typeof userSafe !== 'undefined' &&
+        typeof userSafe === 'boolean') ? userSafe : false;
+    if (userSafe) {
+        message = 'DS_USER:' + message;
+    }
+    throw new Error(message);
 }
